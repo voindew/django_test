018cf70a322db081bb7fa07cd5577c49b8016c1d
voidnew <voidnew@daum.net>
2023-01-11 14:17:37 +0900

    변경 연습

--- django_project1/asgi.py
+++ django_project1/asgi.py
@@ -14,3 +14,6 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project1.settings')
 
 application = get_asgi_application()
+
+
+# asgi.py 주석 추가 
